bc0de6dacca7060cb999ed6139245a1b5f3b51be
analauradias <anamanfrondias@gmail.com>
2024-04-08 16:49:14 -0300

    terminando funcionalidades do formulário

--- src/App.vue
+++ src/App.vue
@@ -44,136 +44,146 @@ let estados = [
   { name: 'SE' },
   { name: 'TO' }
 ]
+
+const enviar = ref(true);
 </script>
 
 <template>
-  <form>
-    <div class="row mb-4">
-      <div class="col">
-        <div data-mdb-input-init class="form-outline">
-          <input type="text" class="form-control" v-model="dados.nome" placeholder="Nome" />
+  <main>
+    <form v-if="enviar">
+      <h1>Crie seu perfil</h1>
+      <div class="row mb-4">
+        <div class="col">
+          <div data-mdb-input-init class="form-outline">
+            <input type="text" class="form-control" v-model="dados.nome" placeholder="Nome" />
+          </div>
+        </div>
+
+        <div class="col">
+          <div data-mdb-input-init class="form-outline">
+            <input type="text" class="form-control" v-model="dados.sobrenome" placeholder="Sobrenome" />
+          </div>
         </div>
       </div>
 
-      <div class="col">
-        <div data-mdb-input-init class="form-outline">
-          <input
-            type="text"
-            class="form-control"
-            v-model="dados.sobrenome"
-            placeholder="Sobrenome"
-          />
+      <!-- EMAIL -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="email" class="form-control" v-model="dados.email" placeholder="E-mail" />
+      </div>
+
+      <!-- SENHA -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="password" class="form-control" v-model="dados.senha" placeholder="Senha" />
+      </div>
+
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="password" class="form-control" v-model="dados.confirmSenha" placeholder="Confirme sua senha" />
+      </div>
+
+      <!-- DATA DE NASCIMENTO -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <label for="dataNasc">Data de nascimento:</label>
+        <input type="date" id="dataNasc" class="form-control" v-model="dados.dataNasc" placeholder="Data de nascimento" />
+      </div>
+
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="text" class="form-control" v-model="dados.endereco" placeholder="Endereço" />
+      </div>
+
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input type="text" class="form-control" v-model="dados.cidade" placeholder="Cidade" />
+      </div>
+
+      <div data-mdb-input-init class="form-outline mb-4">
+        <label class="form-label" for="estado">Estado:</label>
+        <select class="form-select" id="estado" v-model="dados.estado">
+          <option v-for="estado of estados" :key="estado">{{ estado.name }}</option>
+        </select>
+      </div>
+
+      <!-- HOBBIES  -->
+      <section class="separar">
+        <p>Seus Hobbies:</p>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" id="esportes" value="option1" />
+          <label class="form-check-label" for="esportes">Esportes</label>
         </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" id="musica" value="option2" />
+          <label class="form-check-label" for="musica">Música</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" id="leitura" value="option2" />
+          <label class="form-check-label" for="leitura">Leitura</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" id="filme" value="option2" />
+          <label class="form-check-label" for="filme">Filmes/Seriados</label>
+        </div>
+      </section>
+      <!-- LINGUAGENS DE PROGRAMAÇÂO -->
+      <section class="separar">
+        <p>Linguagem preferida:</p>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" id="js" value="option1" v-model="dados.linguagens"/>
+          <label class="form-check-label" for="js">JavaScript</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" id="pyton" value="option2" v-model="dados.linguagens" />
+          <label class="form-check-label" for="pyton">Swift</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" id="php" value="option2" v-model="dados.linguagens"/>
+          <label class="form-check-label" for="php">PHP</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" id="c" value="option2" v-model="dados.linguagens"/>
+          <label class="form-check-label" for="c">C</label>
+        </div>
+      </section>
+
+      <!-- BIOGRAFIA -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <textarea class="form-control" id="form6Example7" rows="4" placeholder="Biografia" v-model="dados.bio"></textarea>
       </div>
-    </div>
 
-    <!-- Text input -->
-    <div data-mdb-input-init class="form-outline mb-4">
-      <input type="email" class="form-control" v-model="dados.email" placeholder="E-mail" />
-    </div>
-
-    <!-- Text input -->
-    <div data-mdb-input-init class="form-outline mb-4">
-      <input type="password" class="form-control" v-model="dados.senha" placeholder="Senha" />
-    </div>
-
-    <!-- Email input -->
-    <div data-mdb-input-init class="form-outline mb-4">
-      <input
-        type="password"
-        class="form-control"
-        v-model="dados.confirmSenha"
-        placeholder="Confirme sua senha"
-      />
-    </div>
-
-    <!-- Number input -->
-    <div data-mdb-input-init class="form-outline mb-4">
-      <input
-        type="date"
-        class="form-control"
-        v-model="dados.dataNasc"
-        placeholder="Data de nascimento"
-      />
-    </div>
-
-    <div data-mdb-input-init class="form-outline mb-4">
-      <input type="text" class="form-control" v-model="dados.endereco" placeholder="Endereço" />
-    </div>
-
-    <div data-mdb-input-init class="form-outline mb-4">
-      <input type="text" class="form-control" v-model="dados.cidade" placeholder="Cidade" />
-    </div>
-
-    <div data-mdb-input-init class="form-outline mb-4">
-      <label class="form-label" for="estado">Estado:</label>
-      <select class="form-select" id="estado" v-model="dados.estado">
-        <option v-for="estado of estados" :key="estado">{{ estado.name }}</option>
-      </select>
-    </div>
-
-    <!-- HOBBIES  -->
-    <section class="separar">
-      <p>Seus Hobbies:</p>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="esportes" value="option1" />
-        <label class="form-check-label" for="esportes">Esportes</label>
+      <!-- ENVIAR -->
+      <button @click="enviar = false" data-mdb-ripple-init type="button"
+        class="btn btn-primary btn-block mb-4">Enviar</button>
+    </form>
+    <section v-else>
+      <div>
+        <h2>Dados:</h2>
+        <p>Nome: {{ dados.nome }}</p>
+        <p>Sobrenome: {{ dados.sobrenome }}</p>
+        <p>E-mail: {{ dados.email }}</p>
+        <p>Senha: {{ dados.senha }}</p>
+        <p>Endereço: {{ dados.endereco }}</p>
+        <p>Cidade: {{ dados.cidade }}</p>
+        <p>Estados: {{ dados.estados }}</p>
+        <p>Hobbies: {{ dados.hobbies }}</p>
+        <p>Linguagem favorita: {{ dados.linguagens }}</p>
+        <p>Biografia: {{ dados.bio }}</p>
       </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="musica" value="option2" />
-        <label class="form-check-label" for="musica">Música</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="leitura" value="option2" />
-        <label class="form-check-label" for="leitura">Leitura</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="filme" value="option2" />
-        <label class="form-check-label" for="filme">Filmes/Seriados</label>
-      </div>
+
+      <button @click="enviar = true" data-mdb-ripple-init type="button"
+        class="btn btn-primary btn-block mb-4">Voltar</button>
     </section>
-    <!-- LINGUAGENS DE PROGRAMAÇÂO -->
-    <section class="separar">
-      <p>Linguagem preferida:</p>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="js" value="option1" />
-        <label class="form-check-label" for="js">JavaScript</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="pyton" value="option2" />
-        <label class="form-check-label" for="pyton">Pyton</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="php" value="option2" />
-        <label class="form-check-label" for="php">PHP</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="c" value="option2" />
-        <label class="form-check-label" for="c">C</label>
-      </div>
-    </section>
-
-    <!-- Message input -->
-    <div data-mdb-input-init class="form-outline mb-4">
-      <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-      <label class="form-label" for="form6Example7">Additional information</label>
-    </div>
-
-    <!-- Checkbox -->
-    <div class="form-check d-flex justify-content-center mb-4">
-      <input class="form-check-input me-2" type="checkbox" value="" id="form6Example8" checked />
-      <label class="form-check-label" for="form6Example8"> Create an account? </label>
-    </div>
-
-    <!-- Submit button -->
-    <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">
-      Place order
-    </button>
-  </form>
+  </main>
 </template>
 
 <style scoped>
+main {
+  margin-left: 50%;
+  width: 70%;
+}
+
 .separar {
   margin-top: 5%;
+  margin-bottom: 5%;
+}
+
+form {
+  margin-top: 10%;
 }
 </style>
